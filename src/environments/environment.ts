932ee0a97d0231911018d3c12c337cc8e6eeed23
pdamort <pdamort359@g.educaand.es>
2022-05-13 19:35:10 +0200

    Comenzamos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,28 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'trailway-6b013',
+    appId: '1:311974001180:web:826e2f9cc54723ab0b7dd9',
+    databaseURL: 'https://trailway-6b013-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'trailway-6b013.appspot.com',
+    apiKey: 'AIzaSyA7c55DBHjkO8TsP7u9BfSUcyW1jYQ0_eU',
+    authDomain: 'trailway-6b013.firebaseapp.com',
+    messagingSenderId: '311974001180',
+    measurementId: 'G-9YZYQHCPLZ',
+  },
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyA7c55DBHjkO8TsP7u9BfSUcyW1jYQ0_eU",
+    authDomain: "trailway-6b013.firebaseapp.com",
+    databaseURL: "https://trailway-6b013-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "trailway-6b013",
+    storageBucket: "trailway-6b013.appspot.com",
+    messagingSenderId: "311974001180",
+    appId: "1:311974001180:web:826e2f9cc54723ab0b7dd9",
+    measurementId: "G-9YZYQHCPLZ"
+  },
 };
 
 /*
